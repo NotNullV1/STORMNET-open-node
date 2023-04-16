685701e314e1b82b83b75e2e5664e60422c3be2d
NotNullV1 <notnullv1@protonmail.ch>
2023-04-16 21:00:27 +0200

    Added Proof of Work validation

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const version = "0.0.2"
 
 const fs = require('fs');
+const crypto = require('crypto');
 
 const server = require('https').createServer({
   key: fs.readFileSync("./cert/key.pem"),
@@ -10,6 +11,12 @@ const server = require('https').createServer({
 var clients = [];
 var receivedMessages = [];
 
+function sha256(input) {
+  const hash = crypto.createHash('sha256');
+  hash.update(input);
+  return hash.digest('hex');
+}
+
 const io = require('socket.io')(server);
 io.on('connection', client => {
   console.log("Connected")
@@ -17,7 +24,21 @@ io.on('connection', client => {
   function processMessage(message) {
     message.from = client.id;
     console.log(message)
-    if(message.messageId==undefined) return;
+    if(message.pow==undefined||message.hash==undefined||message.messageId==undefined) {
+      client.disconnect();
+      return;
+    }
+    var toVerify = {
+      messageId: message.messageId,
+      encryptedMessage: message.encryptedMessage,
+      encryptedKey: message.encryptedKey,
+      publicKey: message.publicKey
+    }
+    if(sha256(JSON.stringify(toVerify)+message.pow)!=message.hash||!message.hash.startsWith("0000")) {
+      // invalid proof of work
+      client.disconnect();
+      return;
+    }
     if(receivedMessages.includes(message.messageId)) return;
     receivedMessages.push(message.messageId)
     clients.forEach(c=>{
